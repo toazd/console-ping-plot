c5dddff125c03b95b571e0dd9c1af9f54c6b6127
toazd <wmcdannell@hotmail.com>
2021-02-17 14:08:42 -0500

    update

--- console-ping-plot.sh
+++ console-ping-plot.sh
@@ -18,13 +18,13 @@
 label_xlabel_samples_color='dark-salmon'
    label_ylabel_time_color='dark-salmon'
         label_jitter_color='dark-spring-green'
-               xtics_color='dark-khaki'
-               ytics_color='dark-khaki'
-              border_color='sienna1'
+               xtics_color='sienna1'
+               ytics_color='sienna1'
+              border_color='goldenrod'
         point_type_average='μ'
-        plot_average_color='royalblue'
+        plot_average_color='dark-cyan'
         point_type_samples='x'
-         plot_sample_color='dark-gray'
+         plot_sample_color='dark-magenta'
 ###############################################################################
 null_response_max=25 # If null_response_count reaches this value the script will abort
 null_response_decay_factor=2 # How much null_response_count will be reduced by (after multiplying by 1) for each successfull ping that follows any ping failure
